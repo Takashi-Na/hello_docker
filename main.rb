7d9dcfcfe2cd52bbb68219af4dd27e269cf69c9b
Takashi-Na <69133170+Takashi-Na@users.noreply.github.com>
2021-10-07 10:13:30 +0900

    Initial commit

---
+++ main.rb
@@ -0,0 +1,14 @@
+# helloを表示しているだけ
+require 'webrick'
+
+server = WEBrick::HTTPServer.new(
+  DocumentRoot: './',
+  BindAddres: '0.0.0.0',
+  Port: 8000
+)
+
+server.mount_proc('/') do |req, res|
+  res.body = 'hello'
+end
+
+server.start
